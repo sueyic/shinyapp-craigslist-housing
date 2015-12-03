e36479ef483682b06fcb818fc32bd35d4b0e059c
Sue Chew <sueyic@gmail.com>
2015-12-03 00:11:13 -0800

    Add price/sqft to the shiny

--- shinyapp_apartment_pricesize_predictor/server.R
+++ shinyapp_apartment_pricesize_predictor/server.R
@@ -4,16 +4,22 @@ pricemod <- readRDS('data/pricemod.rds')
 sqftmod <- readRDS('data/sqftmod.rds')
 
 shinyServer(function(input, output) {
-    
-    
-    output$predictedPrice <- renderText({
+
+    getPredPrice <- reactive({
         test <- data.frame(city=input$selCity, bedrooms=as.numeric(input$selBedrooms))
-        prediction <- predict(pricemod, newdata = test) 
-        paste0('$ ', round(prediction, 2))
+        prediction <- predict(pricemod, newdata = test)
     })
-    output$predictedSize <- renderText({
+
+    getPredSize <- reactive({
         test <- data.frame(city=input$selCity, bedrooms=as.numeric(input$selBedrooms))
         prediction <- predict(sqftmod, newdata = test)
-        paste0(round(prediction, 2), ' sqft')
+    })
+
+    output$predictedPrice <- renderText(paste0('$ ', round(getPredPrice(), 2)))
+
+    output$predictedSize <- renderText(paste0(round(getPredSize(), 2), ' sqft'))
+
+    output$predictedPricePerSize <- renderText({
+        paste0(round(getPredPrice() / getPredSize(), 2), ' $/sqft')
     })
 })
--- shinyapp_apartment_pricesize_predictor/ui.R
+++ shinyapp_apartment_pricesize_predictor/ui.R
@@ -8,15 +8,15 @@ for(c in cities) { cityChoices[[c]] = c }
 bedroomChoices = as.list(1:4)
 
 shinyUI(fluidPage(
-    
+
     # Application title
     titlePanel("Apartment price and size predictor"),
-    
+
     # Sidebar
     sidebarLayout(
         sidebarPanel(
             p("This app will predict the apartment price (USD) and size (sqft) for a given city and desired number of bedrooms."),
-            
+
             p(
                 span("The data is trained from craigslist postings in the 'apts/housing' category, 100 posts from each of several cities. Only several cities are supported. A writeup for how the models were made is available on github "),
                 a("here", href="https://github.com/sueyic/shinyapp-craigslist-housing/blob/master/data_processing/data_processing_report.pdf")
@@ -25,28 +25,33 @@ shinyUI(fluidPage(
 
         mainPanel(
             h2('Select your desired city and number of bedrooms'),
-            
-            radioButtons("selCity", label = h3("Select city"), 
+
+            radioButtons("selCity", label = h3("Select city"),
                         choices = cityChoices, selected = cityChoices[[1]],
                         inline=TRUE),
 
-            radioButtons("selBedrooms", label = h3("Select number of bedrooms"), 
+            radioButtons("selBedrooms", label = h3("Select number of bedrooms"),
                         choices = bedroomChoices, selected = 1,
                         inline=TRUE),
-            
+
             br(),
             h2('Predictions'),
-            
-            p(
-                h3('Predicted price (USD): '),
-                textOutput('predictedPrice')
-            ),
 
-            p(
-                h3('Predicted size: '),
-                textOutput('predictedSize')
+            fluidRow(
+                column(4, div(
+                    h4('Predicted price (USD)'),
+                    textOutput('predictedPrice')
+                )),
+                column(4, div(
+                    h4('Predicted size'),
+                    textOutput('predictedSize')
+                )),
+                column(4, div(
+                    h4('Predicted price/size'),
+                    textOutput('predictedPricePerSize')
+                ))
             )
-            
+
         )
     )
 ))
